f53f9021e30455aba7d7b421ca2433390d92ab86
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2022-12-01 16:47:24 +0100

    Cleanup and use fs instead of BufReader

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -1,5 +1,4 @@
-use std::fs::File;
-use std::io::{self, BufRead};
+use std::fs;
 
 fn main()
 {
@@ -7,49 +6,38 @@ fn main()
     day1("input.txt");
 }
 
-fn read_file_by_line(file_path: &str) -> io::Result<io::Lines<io::BufReader<File>>>
-{
-    let file = File::open(file_path)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
 fn day1(file_path: &str)
 {
     let mut elfs: Vec<u32> = Vec::<u32>::new();
     let mut current_elf: u32 = 0;
-
-    if let Ok(lines) = read_file_by_line(file_path)
+    let input: String = if let Ok(input) = fs::read_to_string(file_path) { input } else { String::from("Loading failed!") };
+    
+    for line in input.lines()
     {
-        for line in lines
+        if let Ok(food) = line.parse::<u32>()
         {
-            if let Ok(line_formatted) = line
-            {
-                if let Ok(food) = line_formatted.parse::<u32>()
-                {
-                    current_elf += food;
-                }
-                else
-                {
-                    elfs.push(current_elf);
-                    current_elf = 0;
-                }
-            }
-        }
-        elfs.push(current_elf);
-
-        elfs.sort();
-        elfs.reverse();
-
-        for calories in &elfs
+            current_elf += food;
+        } else
         {
-            println!("{}", calories)
+            elfs.push(current_elf);
+            current_elf = 0;
         }
-        println!("");
+    };
 
-        println!("The top elf carries food of this many calories: {}", elfs[0]);
+    elfs.push(current_elf);
 
-        let sum_of_top_3: u32 = elfs[0] + elfs[1] + elfs[2];
+    elfs.sort();
+    elfs.reverse();
 
-        println!("The top 3 elves carry: {}", sum_of_top_3);
+    for calories in &elfs
+    {
+        println!("{}", calories)
     }
+    println!("");
+
+    println!("The top elf carries food of this many calories: {}", elfs[0]);
+
+    let sum_of_top_3: u32 = elfs[0] + elfs[1] + elfs[2];
+
+    println!("The top 3 elves carry: {}", sum_of_top_3);
 }
