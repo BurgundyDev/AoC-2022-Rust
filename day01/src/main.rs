15586bb18fdb231cfd2f3b5de936167f7a5f557d
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2022-12-03 08:22:55 +0100

    Finish rename

---
+++ day01/src/main.rs
@@ -0,0 +1,43 @@
+use aoc2022::read_file;
+
+fn main()
+{
+    day01("test_input.txt");
+    day01("input.txt");
+}
+
+fn day01(file_path: &str)
+{
+    let mut elfs: Vec<u32> = Vec::<u32>::new();
+    let mut current_elf: u32 = 0;
+    let input: String = read_file(file_path);
+    
+    for line in input.lines()
+    {
+        if !line.is_empty()
+        {
+            let food = line.parse::<u32>().unwrap();
+            current_elf += food;
+        } else
+        {
+            elfs.push(current_elf);
+            current_elf = 0;
+        }
+    };
+    elfs.push(current_elf);
+
+    elfs.sort();
+    elfs.reverse();
+
+    for calories in &elfs
+    {
+        println!("{}", calories)
+    }
+    println!("");
+
+    println!("The top elf carries food of this many calories: {}", elfs[0]);
+
+    let sum_of_top_3: u32 = elfs[0] + elfs[1] + elfs[2];
+
+    println!("The top 3 elves carry: {}", sum_of_top_3);
+}
---
+++ day02/src/main.rs
@@ -0,0 +1,141 @@
+use aoc2022::read_file;
+
+fn main()
+{
+    part01("test_input.txt");
+    part01("input.txt");
+    part02("test_input.txt");
+    part02("input.txt");
+}
+
+fn part01(file_path: &str) -> u32
+{
+    let mut points: u32 = 0;
+    
+    let input: String = read_file(file_path);
+    
+    for line in input.lines()
+    {
+        if line.contains("A")
+        {
+            println!("Enemy played Rock");
+            if line.contains("X")
+            {
+                println!("You played Rock");
+                points += 1 + 3
+                
+            }else if line.contains("Y")
+            {
+                println!("You played Paper");
+                points += 2 + 6
+            } else if  line.contains("Z")
+            {
+                println!("You played Scissors");
+                points += 3
+            }
+
+        }else if line.contains("B")
+        {
+            println!("Enemy played Paper");
+            if line.contains("X")
+            {
+                println!("You played Rock");
+                points += 1 + 0
+                
+            }else if line.contains("Y")
+            {
+                println!("You played Paper");
+                points += 2 + 3
+            } else if  line.contains("Z")
+            {
+                println!("You played Scissors");
+                points += 3 + 6
+            }
+        } else if  line.contains("C")
+        {
+            println!("Enemy played Scissors");
+            if line.contains("X")
+            {
+                println!("You played Rock");
+                points += 1 + 6
+                
+            }else if line.contains("Y")
+            {
+                println!("You played Paper");
+                points += 2 + 0
+            } else if  line.contains("Z")
+            {
+                println!("You played Scissors");
+                points += 3 + 3
+            }
+        }
+    }
+    println!("{}", points);
+    points
+}
+fn part02(file_path: &str) -> u32
+{
+    let mut points: u32 = 0;
+    let input: String = read_file(file_path);
+    
+    for line in input.lines()
+    {
+        if line.contains("A")
+        {
+            println!("Enemy played Rock");
+
+            if line.contains("X")
+            {
+                println!("You lost. You played Scissors");
+                points += 3
+                
+            }else if line.contains("Y")
+            {
+                println!("You draw. You played Rock");
+                points += 1 + 3
+            } else if  line.contains("Z")
+            {
+                println!("You win. You played Paper");
+                points += 2 + 6
+            }
+
+        }else if line.contains("B")
+        {
+            println!("Enemy played Paper");
+
+            if line.contains("X")
+            {
+                println!("You lost. You played Rock");
+                points += 1 + 0
+                
+            }else if line.contains("Y")
+            {
+                println!("You draw. You played Paper");
+                points += 2 + 3
+            } else if  line.contains("Z")
+            {
+                println!("You won. You played Scissors");
+                points += 3 + 6
+            }
+        } else if  line.contains("C")
+        {
+            println!("Enemy played Scissors");
+
+            if line.contains("X")
+            {
+                println!("You lost. You played Paper");
+                points += 2 + 0
+            }else if line.contains("Y")
+            {
+                println!("You draw. You played Scissors");
+                points += 3 + 3
+            } else if  line.contains("Z")
+            {
+                println!("You won. You played Rock");
+                points += 1 + 6
+            }
+        }
+    }
+    println!("{}", points);
+    points
+}
