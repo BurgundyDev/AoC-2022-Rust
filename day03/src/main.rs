c5366065026693c68bc734640e1c1e0624545a8c
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2022-12-03 09:55:08 +0100

    clean this code up a bit

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -14,21 +14,15 @@ fn part1(file_path: &str) -> i32
 {
     let input = read_file(file_path);
 
-    let mut data: Vec<Chars<>> = Vec::new();
-    let mut fin_data: Vec<Vec<i32>> = Vec::new();
+    // let mut data: Vec<Chars<>> = Vec::new();
+    let mut data: Vec<Vec<i32>> = Vec::new();
 
     let mut sum_of_priorities: i32 = 0;
 
     for line in input.lines()
     {
-        data.push(line.chars())
-    }
-
-    for line in data
-    {
         let mut line_of_ints: Vec<i32> = Vec::<i32>::new();
-
-        for character in line
+        for character in line.chars()
         {
             let mut char_as_int = character as i32;
             char_as_int -= 64;
@@ -42,11 +36,11 @@ fn part1(file_path: &str) -> i32
             // print!("{} ", char_as_int);
             line_of_ints.push(char_as_int)
         }
-        fin_data.push(line_of_ints);
+        data.push(line_of_ints);
         // println!();
     }
 
-    for rucksack in fin_data
+    for rucksack in data
     {
         let (half1, half2) = rucksack.split_at(rucksack.len()/2);
         let mut repeated_numbers = Vec::<i32>::new();
@@ -58,7 +52,7 @@ fn part1(file_path: &str) -> i32
                 if !repeated_numbers.contains(number)
                 {
                     repeated_numbers.push(*number);
-                    println!("{}", number);
+                    // println!("{}", number);
                     sum_of_priorities += *number;
                 }
             }
@@ -73,20 +67,14 @@ fn part2(file_path: &str) -> i32
     let input = read_file(file_path);
 
     let mut data: Vec<Chars<>> = Vec::new();
-    let mut fin_data: Vec<Vec<i32>> = Vec::new();
+    let mut data: Vec<Vec<i32>> = Vec::new();
 
     let mut sum_of_priorities: i32 = 0;
 
     for line in input.lines()
     {
-        data.push(line.chars())
-    }
-
-    for line in data
-    {
         let mut line_of_ints: Vec<i32> = Vec::<i32>::new();
-
-        for character in line
+        for character in line.chars()
         {
             let mut char_as_int = character as i32;
             char_as_int -= 64;
@@ -100,17 +88,17 @@ fn part2(file_path: &str) -> i32
             // print!("{} ", char_as_int);
             line_of_ints.push(char_as_int)
         }
-        fin_data.push(line_of_ints);
+        data.push(line_of_ints);
         // println!();
     }
 
     let mut finit = 0;
-    for rucksack in 0..(fin_data.len()/3)
+    for rucksack in 0..(data.len()/3)
     {
         let mut done = false;
-        for number in &fin_data[finit]
+        for number in &data[finit]
         {
-            if fin_data[finit + 1].contains(number) && fin_data[finit + 2].contains(number) && !done
+            if data[finit + 1].contains(number) && data[finit + 2].contains(number) && !done
             {
                 sum_of_priorities += number;
                 done = true;
