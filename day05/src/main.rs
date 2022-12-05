6707db0cd5da5c3be800123d0d2a1e78e715d525
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2022-12-05 15:21:48 +0100

    Day 5 complete

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -4,11 +4,11 @@ fn main()
 {
     println!("{}", part1("test_input.txt"));
     println!("{}", part1("input.txt"));
-    // println!("{}", part2("test_input.txt"));
-    // println!("{}", part2("input.txt"));
+    println!("{}", part2("test_input.txt"));
+    println!("{}", part2("input.txt"));
 }
 
-fn get_stacks(input: String) -> Vec<Vec<char>>
+fn get_stacks(input: &String) -> Vec<Vec<char>>
 {
     let mut stacks:Vec<Vec<char>> = Vec::new();
     let mut num_of_stacks = input.lines().next().unwrap().len() as u32;
@@ -42,6 +42,11 @@ fn get_stacks(input: String) -> Vec<Vec<char>>
 
     println!("Number of stacks: {} -> {}", num_of_stacks, stacks.len());
 
+    for stack in &mut stacks
+    {
+        stack.reverse();
+    }
+
     let mut row = 1;
     for stack in &stacks
     {
@@ -57,10 +62,111 @@ fn get_stacks(input: String) -> Vec<Vec<char>>
     return stacks
 }
 
+fn get_orders(input: String) -> Vec<Vec<i32>>
+{
+    let mut orders:Vec<Vec<i32>> = Vec::new();
+
+    for line in input.lines()
+    {
+        if line.starts_with("move")
+        {
+            let mut line_formatted = line.replace("move ", "");
+            line_formatted = line_formatted.replace("from ", "");
+            line_formatted = line_formatted.replace("to ", "");
+
+            let order: Vec<i32> = line_formatted.split(" ").map(|n| n.parse().unwrap()).collect();
+            orders.push(order);
+        }
+    }
+
+    orders
+}
+
 fn part1(file_path: &str) -> String
 {
     let input = read_file(file_path);
-    let mut stacks = get_stacks(input);
+    let mut stacks = get_stacks(&input);
 
-    return String::from("");
+    let orders = get_orders(input);
+
+    for order in orders
+    {
+        let mut moves = 0;
+
+        let from: usize = (order[1] - 1) as usize;
+        let to: usize = (order[2] - 1) as usize;
+        while moves < order[0]
+        {
+            let moved_crate = stacks[from].pop().unwrap();
+            stacks[to].push(moved_crate);
+            moves += 1
+        }
+    }
+
+    println!("Stacks after mutation:");
+    let mut stack_num = 0;
+    for stack in &stacks
+    {
+        print!("Stack {}: ", stack_num+1);
+        for character in stack
+        {
+            print!("[{}] ", character);
+        }
+        stack_num += 1;
+        println!()
+    }
+
+    let mut tops = String::from(""); 
+    for stack in &mut stacks
+    {
+        tops += &stack.pop().unwrap().to_string();
+    }
+
+    return tops;
+}
+
+fn part2(file_path: &str) -> String
+{
+    let input = read_file(file_path);
+    let mut stacks = get_stacks(&input);
+
+    let orders = get_orders(input);
+
+    for order in orders
+    {
+        let mut moves = 0;
+
+        let from: usize = (order[1] - 1) as usize;
+        let to: usize = (order[2] - 1) as usize;
+
+        let mut moved_crates: Vec<char> = Vec::<char>::new();
+        while moves < order[0]
+        {
+            moved_crates.push(stacks[from].pop().unwrap());
+            moves += 1
+        }
+        moved_crates.reverse();
+        stacks[to].append(&mut moved_crates);
+    }
+
+    println!("Stacks after mutation:");
+    let mut stack_num = 0;
+    for stack in &stacks
+    {
+        print!("Stack {}: ", stack_num+1);
+        for character in stack
+        {
+            print!("[{}] ", character);
+        }
+        stack_num += 1;
+        println!()
+    }
+
+    let mut tops = String::from(""); 
+    for stack in &mut stacks
+    {
+        tops += &stack.pop().unwrap().to_string();
+    }
+
+    return tops;
 }
